4196b1ca41e0ed06c72647fbad099c9b5f1e04ce
juliq <juliannq@mac.com>
2018-10-13 16:23:44 -0500

    updated Admin page

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class Admin extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                {JSON.stringify(this.props.state)}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Feeling</th>
+                            <th>Comprehension</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.feedback.map(feedback => {
+                            return (
+                                <tr>
+                                    <td>{feedback.feeling}</td>
+                                    <td>{feedback.comprehension}</td>
+                                    <td>{feedback.support}</td>
+                                    <td>{feedback.comments}</td>
+                                    <td>{feedback.delete}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ state });
+
+export default connect(mapStateToProps)(Admin);
